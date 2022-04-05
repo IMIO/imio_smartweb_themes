12191cf1a54922f3022d5665f61a570f48dcbbc3
Thomas Lambert <lambertthomas10@gmail.com>
2022-04-05 09:15:21 +0200

    styles for base theme

--- base/src/index.js
+++ base/src/index.js
@@ -1,1 +1,36 @@
 import "./scss/main.scss";
+
+// // function wrap(el, wrapper) {
+// //     el.parentNode.insertBefore(wrapper, el);
+// //     wrapper.appendChild(el);
+// // }
+
+// // // example: wrapping an anchor with class "wrap_me" into a new div element
+// // wrap(document.getElementById('portal-logo'), document.createElement('div'));
+// // var element = document.createElement("div");
+// // document.getElementById('portal-logo').appendChild(element);  
+
+
+// $( document ).ready(function() {
+//     function wrap(el,el2,el3, wrapper) {
+//         el.parentNode.insertBefore(wrapper,el);
+//         el2.parentNode.insertBefore(wrapper,el2);
+//         el3.parentNode.insertBefore(wrapper,el3);
+//         wrapper.id = 'portal-header-top';
+//         wrapper.appendChild(el);
+//         wrapper.appendChild(el2);
+//         wrapper.appendChild(el3);
+
+//     }
+//     wrap(document.getElementById('portal-logo'), document.getElementById('portal-globalnav-wrapper'), document.getElementById('header-links'), document.createElement('div'));
+
+    
+//     function insertAfter(referenceNode, newNode) {
+//         referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+//       }
+      
+//       var el = document.createElement("div");
+//       el.id = "banner-homepage";
+//       var div = document.getElementById("portal-header-top");
+//       insertAfter(div, el);
+// });
