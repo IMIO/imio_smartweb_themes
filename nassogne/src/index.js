bb789bbbd1a28a4a0b322b7aa737e5880c4aee08
Thomas Lambert <lambertthomas10@gmail.com>
2022-09-21 09:57:20 +0200

    theme for nassogne

--- nassogne/src/index.js
+++ nassogne/src/index.js
@@ -8,4 +8,13 @@ $(document).ready(function () {
     document.body.classList.remove('open-nav-overflow');
     document.documentElement.classList.remove('open-nav-overflow');
   });
+
+    // For custom slider
+    var allSwiper = $('div.swiper')
+    for (var i = 0; i < allSwiper.length; i++) {
+        if(allSwiper[i].getAttribute("data-nb-results-by-batch") > 1){
+            allSwiper[i].swiper.params.spaceBetween=30;
+            allSwiper[i].swiper.update();
+        }
+      }
 });
