ea7a46a0f2fad26fa7337661f17053a80ff4393d
Thomas Lambert <lambertthomas10@gmail.com>
2023-03-17 14:19:03 +0100

    Theme Chastre

--- chastre/src/index.js
+++ chastre/src/index.js
@@ -8,12 +8,4 @@ $( document ).ready(function() {
         document.body.classList.remove('open-nav-overflow');
         document.documentElement.classList.remove('open-nav-overflow');
     })
-
-    $(':not(div[class*="swiper_"]) + div[class*="swiper_"], * > div[class*="swiper_"]:first-of-type').
-    each(function() {
-      $(this).
-          nextUntil(':not(div[class*="swiper_"])').
-          addBack().
-          wrapAll('<div class="swiper_items_wrapper" />');
-    });
 });
