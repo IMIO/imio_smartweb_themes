be22c0e4d479c54b80b2cce940e31e69dd0dd258
thomlamb <lambertthomas10@gmail.ciom>
2023-06-16 11:04:15 +0200

    up webpack dep

--- chastre/src/index.js
+++ chastre/src/index.js
@@ -8,4 +8,24 @@ $( document ).ready(function() {
         document.body.classList.remove('open-nav-overflow');
         document.documentElement.classList.remove('open-nav-overflow');
     })
+
+    // For custom slider
+    if($('.sectionnews .swiper').length > 0){
+        var newsSwiper = $('.sectionnews .swiper')[0].swiper
+        newsSwiper.params.spaceBetween=30;
+        newsSwiper.params.speed=1000,
+        newsSwiper.update();
+    }
+    if($('.sectionevents .swiper').length > 0){
+        var eventsSwiper = $('.sectionevents .swiper')[0].swiper
+        eventsSwiper.params.spaceBetween=30;
+        eventsSwiper.params.speed=1000,
+        eventsSwiper.update();
+    }
+    if($('.sectioncollection .swiper').length > 0){
+        var collecSwiper = $('.sectioncollection .swiper')[0].swiper
+        collecSwiper.params.spaceBetween=60;
+        collecSwiper.params.speed=1000,
+        collecSwiper.update();
+    }
 });
