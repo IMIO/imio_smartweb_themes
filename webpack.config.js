2fe315bc037c56649982c2eaba191c95938def89
thomlamb <lambertthomas10@gmail.ciom>
2023-07-07 13:43:26 +0200

    Add progress bar for webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require("path");
-
+const WebpackBar = require('webpackbar');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
@@ -79,6 +79,7 @@ module.exports = (env, argv) => {
       new MiniCssExtractPlugin({
         filename: "dist/css/theme.css",
       }),
+      new WebpackBar(),
     ].filter(Boolean),
     module: {
       rules: [
