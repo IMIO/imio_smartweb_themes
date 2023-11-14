aedec29fb95139d34dee1632328cd43e4e4a6a1f
thomlamb <lambertthomas10@gmail.com>
2023-11-14 15:56:14 +0100

    Fix remicourts

--- remicourt/src/index.js
+++ remicourt/src/index.js
@@ -23,12 +23,4 @@ $( document ).ready(function() {
         eventsSwiper.update();
     }
 
-    // For banner
-
-    let checkBanner = document.querySelector("#portal-header #banner");
-    let headerCustom = document.getElementById("portal-header");
-
-    if (checkBanner != null) {
-       headerCustom.classList.add("header-custom");
-    }
 });
