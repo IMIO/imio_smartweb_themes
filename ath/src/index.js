d942f86ec40dafd835f6f2afecc2a979a58f112b
jerhero <jeremy.hero@imio.be>
2024-02-08 21:00:37 +0100

    Theme for Ath

--- ath/src/index.js
+++ ath/src/index.js
@@ -1,5 +1,4 @@
 import "./scss/main.scss";
-    
 $( document ).ready(function() {
     $('#portal-globalnav-collapse').on('show.bs.collapse', function () {
         document.body.classList.add('open-nav-overflow');
@@ -9,5 +8,27 @@ $( document ).ready(function() {
         document.body.classList.remove('open-nav-overflow');
         document.documentElement.classList.remove('open-nav-overflow');
     })
-    
+
+    // For custom slider
+    if($('.sectionnews .swiper').length > 0){
+        var newsSwiper = $('.sectionnews .swiper')[0].swiper
+        newsSwiper.params.spaceBetween=30;
+        newsSwiper.params.speed=1000,
+        newsSwiper.update();
+    }
+    if($('.sectionevents .swiper').length > 0){
+        var eventsSwiper = $('.sectionevents .swiper')[0].swiper
+        eventsSwiper.params.spaceBetween=30;
+        eventsSwiper.params.speed=1000,
+        eventsSwiper.update();
+    }
+
+    // For banner
+
+    let checkBanner = document.querySelector("#portal-header #banner");
+    let headerCustom = document.getElementById("portal-header");
+
+    if (checkBanner != null) {
+       headerCustom.classList.add("header-custom");
+    }
 });
