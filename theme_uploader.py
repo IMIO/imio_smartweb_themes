2506d47ff58f7b9eb6955f86c63c51c5cce99c49
rdubois <remi.dubois@imio.be>
2024-07-23 14:09:59 +0200

    feat(gha): upload base theme
    WEB-4112

---
+++ theme_uploader.py
@@ -0,0 +1,66 @@
+from bs4 import BeautifulSoup
+
+import requests
+import requests.cookies
+import sys
+
+INSTANCE_URL = sys.argv[1]
+USERNAME = sys.argv[2]
+PASSWORD = sys.argv[3]
+THEME_LOCATION = "base/theme.zip"
+
+
+def authenticate(
+    session: requests.Session, login_url: str, username: str, password: str
+) -> requests.Response:
+    url = INSTANCE_URL + "/failsafe_login"
+    data = {
+        "__ac_name": USERNAME,
+        "__ac_password": PASSWORD,
+        "came_from": f"{INSTANCE_URL}/@@theming-controlpanel",
+        "buttons.login": "Se connecter",
+    }
+    response = session.post(url, data=data)
+    return response
+
+
+def get_cookie(response: requests.Response) -> requests.cookies.RequestsCookieJar:
+    return response.cookies
+
+
+def get_token(response: requests.Response) -> str:
+    soup = BeautifulSoup(response.text, "html.parser")
+    authenticator = soup.find(attrs={"name": "_authenticator"})
+    return authenticator["value"]
+
+
+def upload_theme(
+    session: requests.Session,
+    instance_url: str,
+    token: str,
+    theme_location: str,
+) -> requests.Response:
+    url = instance_url + "/@@theming-controlpanel"
+    with open(theme_location, "rb") as file_content:
+        file = {"themeArchive": ("theme.zip", file_content, "application/zip")}
+        data = {
+            "replaceExisting:boolean": "1",
+            "form.button.Import": "1",
+            "enableNewTheme:boolean": "1",
+            "_authenticator": token,
+        }
+        response = session.post(url, files=file, data=data)
+    return response
+
+
+def main():
+    session = requests.Session()
+    response = authenticate(session, INSTANCE_URL, USERNAME, PASSWORD)
+    token = get_token(response)
+    response = upload_theme(session, INSTANCE_URL, token, THEME_LOCATION)
+    if response.status_code == 200:
+        print("Theme uploaded successfully")
+
+
+if __name__ == "__main__":
+    main()
