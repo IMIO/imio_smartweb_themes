5e64ab9ee6f441a1745148c7d72a0a7e43d16468
Broemman <brotelle.emmanuel@gmail.com>
2025-02-27 14:00:01 +0100

    Theme Lasne - maj test

--- lasne/src/index.js
+++ lasne/src/index.js
@@ -1,22 +1,22 @@
-import "./scss/main.scss";
+import './scss/main.scss';
 $(document).ready(function () {
-  $("#portal-globalnav-collapse").on("show.bs.collapse", function () {
-    document.body.classList.add("open-nav-overflow");
-    document.documentElement.classList.add("open-nav-overflow");
+  $('#portal-globalnav-collapse').on('show.bs.collapse', function () {
+    document.body.classList.add('open-nav-overflow');
+    document.documentElement.classList.add('open-nav-overflow');
   });
-  $("#portal-globalnav-collapse").on("hidden.bs.collapse", function () {
-    document.body.classList.remove("open-nav-overflow");
-    document.documentElement.classList.remove("open-nav-overflow");
+  $('#portal-globalnav-collapse').on('hidden.bs.collapse', function () {
+    document.body.classList.remove('open-nav-overflow');
+    document.documentElement.classList.remove('open-nav-overflow');
   });
 
   // For custom slider
-  if ($(".sectionnews .swiper").length > 0) {
-    var newsSwiper = $(".sectionnews .swiper")[0].swiper;
+  if ($('.sectionnews .swiper').length > 0) {
+    var newsSwiper = $('.sectionnews .swiper')[0].swiper;
     newsSwiper.params.spaceBetween = 30;
     (newsSwiper.params.speed = 1000), newsSwiper.update();
   }
-  if ($(".sectionevents .swiper").length > 0) {
-    var eventsSwiper = $(".sectionevents .swiper")[0].swiper;
+  if ($('.sectionevents .swiper').length > 0) {
+    var eventsSwiper = $('.sectionevents .swiper')[0].swiper;
     eventsSwiper.params.spaceBetween = 30;
     (eventsSwiper.params.speed = 1000), eventsSwiper.update();
   }
