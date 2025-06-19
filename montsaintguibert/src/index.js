b5d0e8f5f499781de4dd9cdb3e90e88373f36d32
Broemman <brotelle.emmanuel@gmail.com>
2025-06-19 06:20:15 +0200

    Theme MSG - index js

---
+++ montsaintguibert/src/index.js
@@ -0,0 +1,21 @@
+import "./scss/main.scss";
+$(document).ready(function () {
+  $("#portal-globalnav-collapse").on("show.bs.collapse", function () {
+    document.body.classList.add("open-nav-overflow");
+    document.documentElement.classList.add("open-nav-overflow");
+  });
+  $("#portal-globalnav-collapse").on("hidden.bs.collapse", function () {
+    document.body.classList.remove("open-nav-overflow");
+    document.documentElement.classList.remove("open-nav-overflow");
+  });
+
+  // For custom slider
+  // var newsSwiper = $('.sectionnews .swiper')[0].swiper
+  // newsSwiper.params.spaceBetween=30;
+  // newsSwiper.params.speed=1000,
+  // newsSwiper.update();
+  // var newsSwiper = $('.sectionevents .swiper')[0].swiper
+  // newsSwiper.params.spaceBetween=30;
+  // newsSwiper.params.speed=1000,
+  // newsSwiper.update();
+});
