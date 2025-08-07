e97c823367de6326e74ee95d37f4c23799922a9b
Broemman <brotelle.emmanuel@gmail.com>
2025-08-07 14:43:59 +0200

    Theme Huy - Homepage

--- huy/src/index.js
+++ huy/src/index.js
@@ -1,18 +1,18 @@
-import './scss/main.scss';
+import "./scss/main.scss";
 $(document).ready(function () {
-  $('#portal-globalnav-collapse').on('show.bs.collapse', function () {
-    document.body.classList.add('open-nav-overflow');
-    document.documentElement.classList.add('open-nav-overflow');
+  $("#portal-globalnav-collapse").on("show.bs.collapse", function () {
+    document.body.classList.add("open-nav-overflow");
+    document.documentElement.classList.add("open-nav-overflow");
   });
-  $('#portal-globalnav-collapse').on('hidden.bs.collapse', function () {
-    document.body.classList.remove('open-nav-overflow');
-    document.documentElement.classList.remove('open-nav-overflow');
+  $("#portal-globalnav-collapse").on("hidden.bs.collapse", function () {
+    document.body.classList.remove("open-nav-overflow");
+    document.documentElement.classList.remove("open-nav-overflow");
   });
 
   // For custom slider
-  var allSwiper = $('div.swiper');
+  var allSwiper = $("div.swiper");
   for (var i = 0; i < allSwiper.length; i++) {
-    if (allSwiper[i].getAttribute('data-nb-results-by-batch') > 1) {
+    if (allSwiper[i].getAttribute("data-nb-results-by-batch") > 1) {
       allSwiper[i].swiper.params.spaceBetween = 30;
       allSwiper[i].swiper.update();
     }
