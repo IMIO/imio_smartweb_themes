1c8e875ae6e726931ef0179ae42beeb6e621860a
thomlamb <lambertthomas10@gmail.com>
2026-01-15 15:35:06 +0100

    Prettier update

--- wellin/src/index.js
+++ wellin/src/index.js
@@ -1,12 +1,12 @@
-import "./scss/main.scss";
+import './scss/main.scss';
 $(document).ready(function () {
-  $("#portal-globalnav-collapse").on("show.bs.collapse", function () {
-    document.body.classList.add("open-nav-overflow");
-    document.documentElement.classList.add("open-nav-overflow");
+  $('#portal-globalnav-collapse').on('show.bs.collapse', function () {
+    document.body.classList.add('open-nav-overflow');
+    document.documentElement.classList.add('open-nav-overflow');
   });
-  $("#portal-globalnav-collapse").on("hidden.bs.collapse", function () {
-    document.body.classList.remove("open-nav-overflow");
-    document.documentElement.classList.remove("open-nav-overflow");
+  $('#portal-globalnav-collapse').on('hidden.bs.collapse', function () {
+    document.body.classList.remove('open-nav-overflow');
+    document.documentElement.classList.remove('open-nav-overflow');
   });
 
   $(
