b453629716799b5e609524a834eb251e8075b2f7
thomlamb <lambertthomas10@gmail.com>
2026-03-10 15:26:13 +0100

    base theme css for search improvement

--- webpack.config.js
+++ webpack.config.js
@@ -52,6 +52,8 @@ module.exports = (env, argv) => {
                       "**/*.zip",
                       "**/src",
                       "**/node_modules",
+                      "**/CHANGELOG.md",
+                      "**/update-changelog.js",
                     ],
                   },
                 },
